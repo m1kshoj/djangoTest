0500e3600c689d25f75d89556075d9cb75e08f0c
m1kshoj <mhlsntsn@icloud.com>
2022-04-19 14:27:19 +0300

    db form for django admin

--- firstPr/news/models.py
+++ firstPr/news/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Articles(models.Model):
-    title = models.CharField('Naming', max_length=50, default='title')
-    anons = models.CharField('Intro', max_length=250, default='anons')
+    title = models.CharField('Naming', max_length=50)
+    anons = models.CharField('Intro', max_length=250)
     full_text = models.TextField('Text')
     date = models.DateTimeField('Publication date')
 
--- firstPr/news/views.py
+++ firstPr/news/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Articles
+from .forms import ArticleFrom
 
 
 def news_home(request):
-    news = Articles.objects.order_by('title')
-
+    news = Articles.objects.order_by('-date')
     return render(request, 'news/news_home.html', {'news': news})
 
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = ArticleFrom(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('news_home')
+        else:
+            error = 'Wrong form'
+
+    form = ArticleFrom()
+    data = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'news/create.html', data)
